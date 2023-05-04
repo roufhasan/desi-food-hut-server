a30fa9cf905f077ea5a87ced77c2626c3005dd1d
roufhasane <roufhasan5@gmail.comm>
2023-05-04 08:50:19 +0600

    express js installed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const chefs = require("./data/chefsData.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Desi Food Hut Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Desi Food Hut API is running on port ${port}`);
+});
