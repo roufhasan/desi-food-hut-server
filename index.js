8c5e5e2383ab10476e840bf624236bc0656283ea
roufhasane <roufhasan5@gmail.comm>
2023-05-04 08:54:54 +0600

    chefs path created

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chefs = require("./data/chefsData.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Desi Food Hut Is Running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
